a35bbbdb910e53a2248e53f224814ad113a7ad8e
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 14:11:42 +0700

    ห

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
@@ -14,34 +14,18 @@ class ManeerinCodeBuyer(models.Model):
     _name = 'maneerincode.buyer'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
     phone = fields.Char(string="Phone")
-
-    @api.onchange('partner_id')
-    def _onchange_partner_id(self):
-        if self.partner_id:
-            self.age = self.partner_id.age or 0  # หรือใช้ค่า default อื่นๆ
-            self.nation = self.partner_id.country_id.name if self.partner_id.country_id else ''
-            self.street = self.partner_id.street or ''
-            self.phone = self.partner_id.phone or ''
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
     phone = fields.Char(string="Phone")
-
-    @api.onchange('partner_id')
-    def _onchange_partner_id(self):
-        if self.partner_id:
-            self.age = self.partner_id.age or 0  # หรือใช้ค่า default อื่นๆ
-            self.nation = self.partner_id.country_id.name if self.partner_id.country_id else ''
-            self.street = self.partner_id.street or ''
-            self.phone = self.partner_id.phone or ''
