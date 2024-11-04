3c9de2e6c56144aa162dcd10bac62fcafbe36433
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 10:44:12 +0700

    u

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -1,10 +1,9 @@
 from odoo import models, fields
-from datetime import datetime
 
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
 
-    contact_date = fields.Datetime(string='Contact Date', default=datetime.now)
+    contact_date = fields.Datetime(string='Contact Date', default=fields.Datetime.now)  # เปลี่ยนเป็น fields.Datetime.now
     seller = fields.Char(string="ผู้จะขาย")
-    buyer = fields.Char(string="ผู้จะซื้อ")
+    buyer = fields.Char(string="ผู้จะซื้อ")  
     beneficiary = fields.Char(string="ผู้รับสิทธิ์")
