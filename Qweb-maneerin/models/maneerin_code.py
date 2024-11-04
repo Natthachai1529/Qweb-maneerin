e72f8ac3c90f0ef04bb6c2d31c6402f96cd8143b
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 12:23:22 +0700

    s

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -6,24 +6,5 @@ class ManeerinCode(models.Model):
     contract_no = fields.Char(string="เลขที่สัญญา")  
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
     seller = fields.Char(string="ผู้จะขาย")
-    buyer_id = fields.Many2one('buyer.info', string="ผู้จะซื้อ")  
-    beneficiary_id = fields.Many2one('beneficiary.info', string="ผู้รับสิทธิ์")
-
-class Buyer(models.Model):
-    _name = 'buyer.info'
-    
-    name = fields.Char(string="ชื่อ", required=True)
-    age = fields.Integer(string="อายุ")
-    nation = fields.Char(string="สัญชาติ")
-    street = fields.Char(string="ถนน")
-    phone = fields.Char(string="โทรศัพท์")
-
-class Beneficiary(models.Model):
-    _name = 'beneficiary.info'
-    
-    name = fields.Char(string="ชื่อ", required=True)
-    age = fields.Integer(string="อายุ")
-    nation = fields.Char(string="สัญชาติ")
-    street = fields.Char(string="ถนน")
-    phone = fields.Char(string="โทรศัพท์")
-
+    buyer_id = fields.Many2one('res.partner', string="ผู้จะซื้อ")  
+    beneficiary_id = fields.Many2one('res.partner', string="ผู้รับสิทธิ์")
