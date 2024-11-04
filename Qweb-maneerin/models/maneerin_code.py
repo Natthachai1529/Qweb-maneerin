755ca3562fbb3385969576f6fe93ef0b3057102e
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 13:37:13 +0700

    หฟ

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -7,7 +7,6 @@ class ManeerinCode(models.Model):
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
     seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")  # ผู้ขายเป็น res.partner
 
-    # ใช้ One2many เพื่อเชื่อมโยงไปยังโมเดลที่สร้างขึ้น
     buyer_ids = fields.One2many('maneerincode.buyer', 'maneerincode_id', string="ผู้จะซื้อ")  
     beneficiary_ids = fields.One2many('maneerincode.beneficiary', 'maneerincode_id', string="ผู้รับสิทธิ์") 
 
@@ -16,19 +15,21 @@ class ManeerinCodeBuyer(models.Model):
     
     maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
     partner_id = fields.Many2one('res.partner', string="ผู้ซื้อ")  # ฟิลด์เพื่อเชื่อมโยงกับ res.partner
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
+
+    name = fields.Char(related='partner_id.name', string="Name", store=True)
+    age = fields.Integer(related='partner_id.age', string="Age", store=True)
+    nation = fields.Char(related='partner_id.nation', string="Nation", store=True)
+    street = fields.Char(related='partner_id.street', string="Street", store=True)
+    phone = fields.Char(related='partner_id.phone', string="Phone", store=True)
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
     partner_id = fields.Many2one('res.partner', string="ผู้รับสิทธิ์")  # ฟิลด์เพื่อเชื่อมโยงกับ res.partner
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
+
+    name = fields.Char(related='partner_id.name', string="Name", store=True)
+    age = fields.Integer(related='partner_id.age', string="Age", store=True)
+    nation = fields.Char(related='partner_id.nation', string="Nation", store=True)
+    street = fields.Char(related='partner_id.street', string="Street", store=True)
+    phone = fields.Char(related='partner_id.phone', string="Phone", store=True)
