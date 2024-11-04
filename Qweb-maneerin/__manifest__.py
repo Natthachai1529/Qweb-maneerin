1b53dbd367375babb36f7b7d4e5d5afeb245b0ce
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 17:58:56 +0700

    87

--- Qweb-maneerin/__manifest__.py
+++ Qweb-maneerin/__manifest__.py
@@ -7,7 +7,7 @@
         'security/ir.model.access.csv',
         'data/data.xml',
         'views/views.xml',
-        'views/maneerin_contract.xml',  # เพิ่มไฟล์นี้
+        'views/maneerin_contract.xml',  
     ],
 
     'application': True,
