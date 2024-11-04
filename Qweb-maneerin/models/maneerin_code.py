774c889cf972993df3a944a3bbbeba2c15b8aec2
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 13:02:59 +0700

    ห

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -6,5 +6,26 @@ class ManeerinCode(models.Model):
     contract_no = fields.Char(string="เลขที่สัญญา")  
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
     seller = fields.Char(string="ผู้จะขาย")
-    buyer_id = fields.Many2one('res.partner', string="ผู้จะซื้อ")  
-    beneficiary_id = fields.Many2one('res.partner', string="ผู้รับสิทธิ์")
+    
+    buyer_ids = fields.One2many('maneerincode.buyer', 'maneerincode_id', string="ผู้จะซื้อ")  
+    beneficiary_ids = fields.One2many('maneerincode.beneficiary', 'maneerincode_id', string="ผู้รับสิทธิ์") 
+
+class ManeerinCodeBuyer(models.Model):
+    _name = 'maneerincode.buyer'
+    
+    maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone")
+
+class ManeerinCodeBeneficiary(models.Model):
+    _name = 'maneerincode.beneficiary'
+    
+    maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
+    name = fields.Char(string="Name")
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone")
