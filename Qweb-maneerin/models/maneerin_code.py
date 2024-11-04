8e17b1aade038dccff7c78c8e34063c8e079dbb8
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 15:32:34 +0700

    หกดดกห

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -3,32 +3,32 @@ from odoo import models, fields, api
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
 
-    contract_no = fields.Char(string="เลขที่สัญญา")  
-    contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
+    contract_no = fields.Char(string="เลขที่สัญญา")
+    contact_date = fields.Date(string='Contact Date', default=fields.Date.today)
     seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")
-
+    
     buyer_ids = fields.One2many(comodel_name='maneerincode.buyer', inverse_name='maneerincode_id', string="ผู้จะซื้อ")  
-    beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์") 
+    beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์")
 
     def action_print_report(self):
-        return self.env.ref('your_module_name.report_maneerincode').report_action(self)
+        return self.env.ref('maneerin.report_maneerincode').report_action(self)
 
 class ManeerinCodeBuyer(models.Model):
     _name = 'maneerincode.buyer'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)
+    name = fields.Many2one(comodel_name='res.partner', string="Name")
+    age = fields.Integer(string="Age", related='name.age', store=True)
+    nation = fields.Char(string="Nation", related='name.nationality', store=True)
+    street = fields.Char(string="Street", related='name.street', store=True)
+    phone = fields.Char(string="Phone", related='name.phone', store=True)
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)
+    name = fields.Many2one(comodel_name='res.partner', string="Name")
+    age = fields.Integer(string="Age", related='name.age', store=True)
+    nation = fields.Char(string="Nation", related='name.nationality', store=True)
+    street = fields.Char(string="Street", related='name.street', store=True)
+    phone = fields.Char(string="Phone", related='name.phone', store=True)
