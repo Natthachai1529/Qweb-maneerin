30149540503566790f95edb389976542e32fc755
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 13:15:05 +0700

    ห

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -3,29 +3,8 @@ from odoo import models, fields
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
 
-    contract_no = fields.Char(string="เลขที่สัญญา")  
-    contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
-    seller = fields.Char(string="ผู้จะขาย")
-    
-    buyer_ids = fields.One2many('maneerincode.buyer', 'maneerincode_id', string="ผู้จะซื้อ")  
-    beneficiary_ids = fields.One2many('maneerincode.beneficiary', 'maneerincode_id', string="ผู้รับสิทธิ์") 
-
-class ManeerinCodeBuyer(models.Model):
-    _name = 'maneerincode.buyer'
-    
-    maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
-
-class ManeerinCodeBeneficiary(models.Model):
-    _name = 'maneerincode.beneficiary'
-    
-    maneerincode_id = fields.Many2one('maneerincode.code', string="Maneerin Code")
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
+    contract_no = fields.Char(string="เลขที่สัญญา")
+    contact_date = fields.Date(string='Contact Date', default=fields.Date.today)
+    seller_id = fields.Many2one('res.partner', string="ผู้จะขาย")  # เพิ่มฟิลด์นี้
+    buyer_ids = fields.One2many('buyer.model', 'maneerin_code_id', string="ผู้จะซื้อ")  # ให้แน่ใจว่าฟิลด์นี้ถูกสร้างแล้ว
+    beneficiary_ids = fields.One2many('beneficiary.model', 'maneerin_code_id', string="ผู้รับสิทธิ์")  # ให้แน่ใจว่าฟิลด์นี้ถูกสร้างแล้ว
