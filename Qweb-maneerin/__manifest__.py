112fe44e999f1aa570f422e4c0a610b7e15fb0e7
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 22:03:14 +0700

    ฟหก

--- Qweb-maneerin/__manifest__.py
+++ Qweb-maneerin/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Qweb',
     'summary': 'Module For Qweb maneerin',
     'description': 'This module manages Qweb reports for Maneerin Codes.',
-    'depends': ['base', 'contacts'],  
+    'depends': ['base', 'contact'],  
     'data': [
         'security/ir.model.access.csv',
         'button/ButtonTag.xml',
