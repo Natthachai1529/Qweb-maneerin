5e8f75151eac28959d599e39dd2eb2df5fb1f1a3
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 15:10:24 +0700

    ย

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -10,32 +10,22 @@ class ManeerinCode(models.Model):
     buyer_ids = fields.One2many(comodel_name='maneerincode.buyer', inverse_name='maneerincode_id', string="ผู้จะซื้อ")  
     beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์") 
 
-    def action_print_report(self):
-        # ฟังก์ชันปริ้นรายงาน
-        return {
-            'type': 'ir.actions.report',
-            'report_name': 'your_module_name.report_template_name',  # ใส่ชื่อของ template ที่จะใช้
-            'report_type': 'qweb-pdf',
-            'model': 'maneerincode.code',
-            'res_id': self.id,
-        }
-
 class ManeerinCodeBuyer(models.Model):
     _name = 'maneerincode.buyer'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)
+    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ใช้ related กับ partner_id
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)
+    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ใช้ related กับ partner_id
