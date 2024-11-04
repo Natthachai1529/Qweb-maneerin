05559524cd653f1e755a0c9562cb9946d994c51d
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 15:22:28 +0700

    sdf

--- Qweb-maneerin/__manifest__.py
+++ Qweb-maneerin/__manifest__.py
@@ -4,7 +4,8 @@
     'depends': ['base', 'contacts'],  
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',  
+        'views/views.xml', 
+        'data/action_data.xml' 
         'reports/report_maneerincode.xml',
     ],
     'application': True,
