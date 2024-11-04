73071bb5bef05ccfc8fdaf1875493cd68b2fb99d
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 12:01:42 +0700

    9

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
 
-    contract_no = fields.Char(string="เลขที่สัญญา")  # แก้ชื่อฟิลด์ให้เป็น snake_case
+    contract_no = fields.Char(string="เลขที่สัญญา")  
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
     seller = fields.Char(string="ผู้จะขาย")
     buyer = fields.Char(string="ผู้จะซื้อ")  
