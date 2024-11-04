6c6bea7b1dd442160ee2a4c906e6c2c274e56bfa
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 14:27:40 +0700

    ฟหก

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
@@ -15,17 +15,33 @@ class ManeerinCodeBuyer(models.Model):
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
     partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
-    age = fields.Integer(string="Age", related='partner_id.age', readonly=True)  # ใช้ related
-    nation = fields.Char(string="Nation", related='partner_id.nationality', readonly=True)  # ใช้ related
-    street = fields.Char(string="Street", related='partner_id.street', readonly=True)  # ใช้ related
-    phone = fields.Char(string="Phone", related='partner_id.phone', readonly=True)  # ใช้ related
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone")
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.age = self.partner_id.age  # ปรับให้ดึงข้อมูลอายุจาก res.partner
+            self.nation = self.partner_id.nationality  # ปรับให้ดึงข้อมูลสัญชาติ
+            self.street = self.partner_id.street  # ปรับให้ดึงข้อมูลถนน
+            self.phone = self.partner_id.phone  # ปรับให้ดึงข้อมูลโทรศัพท์
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
     partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
-    age = fields.Integer(string="Age", related='partner_id.age', readonly=True)  # ใช้ related
-    nation = fields.Char(string="Nation", related='partner_id.nationality', readonly=True)  # ใช้ related
-    street = fields.Char(string="Street", related='partner_id.street', readonly=True)  # ใช้ related
-    phone = fields.Char(string="Phone", related='partner_id.phone', readonly=True)  # ใช้ related
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone")
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.age = self.partner_id.age  # ปรับให้ดึงข้อมูลอายุจาก res.partner
+            self.nation = self.partner_id.nationality  # ปรับให้ดึงข้อมูลสัญชาติ
+            self.street = self.partner_id.street  # ปรับให้ดึงข้อมูลถนน
+            self.phone = self.partner_id.phone  # ปรับให้ดึงข้อมูลโทรศัพท์
