391a0a62cf14ddcb3641a090dbd50e3cb13fecda
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-05 00:20:49 +0700

    ผปแ

--- Qweb-maneerin/models/addon_contact.py
+++ Qweb-maneerin/models/addon_contact.py
@@ -4,5 +4,5 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     age = fields.Integer(string='Age')
-    nation = fields.Char(string='Nationality')
+    nationality = fields.Char(string='Nationality')
     identification_number = fields.Char(string='Identification Number')
