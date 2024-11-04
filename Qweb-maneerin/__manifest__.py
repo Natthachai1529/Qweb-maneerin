90debfaa3a26ac9c64244526528ba6bad7154380
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 14:44:10 +0700

    yghtf

--- Qweb-maneerin/__manifest__.py
+++ Qweb-maneerin/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Qweb',
     'summary': 'Module For Qweb maneerin',
-    'depends': ['base', 'contacts', 'web', 'report'],  
+    'depends': ['base', 'contacts'],  
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',  
