92907c34a094253cff0a5686c395944be29e39a4
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 17:07:06 +0700

    หกฟ

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -3,10 +3,10 @@ from odoo import models, fields
 class ManeerinCode(models.Model):
     _name = 'maneerincode.code'
 
-    contract_no = fields.Char(string="เลขที่สัญญา")
-    contact_date = fields.Date(string='Contact Date', default=fields.Date.today)
-    seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")  # ผู้ขายเป็น res.partner
-    
+    contract_no = fields.Char(string="เลขที่สัญญา")  
+    contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
+    seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")
+
     buyer_ids = fields.One2many(comodel_name='maneerincode.buyer', inverse_name='maneerincode_id', string="ผู้จะซื้อ")  
     beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์") 
 
@@ -14,12 +14,18 @@ class ManeerinCodeBuyer(models.Model):
     _name = 'maneerincode.buyer'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้จะซื้อ")  # เลือกจาก res.partner
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ดึงเบอร์โทรศัพท์จาก res.partner
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ใช้ related กับ partner_id
 
 class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  # เลือกจาก res.partner
-    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ดึงเบอร์โทรศัพท์จาก res.partner
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  # เปลี่ยนเป็น Many2one เพื่อเลือกจาก res.partner
+    age = fields.Integer(string="Age")
+    nation = fields.Char(string="Nation")
+    street = fields.Char(string="Street")
+    phone = fields.Char(string="Phone", related='partner_id.phone', store=True)  # ใช้ related กับ partner_id
