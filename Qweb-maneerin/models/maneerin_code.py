e93120499afb2566617cc6951086b007b33513c1
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 15:38:52 +0700

    dsf

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -6,18 +6,17 @@ class ManeerinCode(models.Model):
     contract_no = fields.Char(string="เลขที่สัญญา")  
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
     seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")
-
     buyer_ids = fields.One2many(comodel_name='maneerincode.buyer', inverse_name='maneerincode_id', string="ผู้จะซื้อ")  
     beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์") 
 
-    def print_report(self):
+    def action_print_report(self):
         return self.env.ref('your_module_name.report_maneerincode').report_action(self)
 
 class ManeerinCodeBuyer(models.Model):
     _name = 'maneerincode.buyer'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")  
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้ซื้อ")
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
@@ -27,7 +26,7 @@ class ManeerinCodeBeneficiary(models.Model):
     _name = 'maneerincode.beneficiary'
     
     maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")  
+    partner_id = fields.Many2one(comodel_name='res.partner', string="ผู้รับสิทธิ์")
     age = fields.Integer(string="Age")
     nation = fields.Char(string="Nation")
     street = fields.Char(string="Street")
