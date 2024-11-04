9f4f8b6e034f8e1448b096d844e19fe453844adb
Natthachai1529 <154317757+Natthachai1529@users.noreply.github.com>
2024-11-04 13:31:16 +0700

    s

--- Qweb-maneerin/models/maneerin_code.py
+++ Qweb-maneerin/models/maneerin_code.py
@@ -5,28 +5,9 @@ class ManeerinCode(models.Model):
 
     contract_no = fields.Char(string="เลขที่สัญญา")  
     contact_date = fields.Date(string='Contact Date', default=fields.Date.today) 
-    seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")  # แก้ไขเป็น comodel_name
+    seller = fields.Many2one(comodel_name="res.partner", string="ผู้จะขาย")  # ผู้ขายเป็น res.partner
     
-    # แก้ไขการเรียกใช้งาน One2many ให้ถูกต้อง
-    buyer_ids = fields.One2many(comodel_name='maneerincode.buyer', inverse_name='maneerincode_id', string="ผู้จะซื้อ")  
-    beneficiary_ids = fields.One2many(comodel_name='maneerincode.beneficiary', inverse_name='maneerincode_id', string="ผู้รับสิทธิ์") 
+    # เปลี่ยนเป็น Many2many เพื่อเชื่อมโยงกับ res.partner
+    buyer_ids = fields.Many2many(comodel_name='res.partner', string="ผู้จะซื้อ")  
+    beneficiary_ids = fields.Many2many(comodel_name='res.partner', string="ผู้รับสิทธิ์") 
 
-class ManeerinCodeBuyer(models.Model):
-    _name = 'maneerincode.buyer'
-    
-    maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
-
-class ManeerinCodeBeneficiary(models.Model):
-    _name = 'maneerincode.beneficiary'
-    
-    maneerincode_id = fields.Many2one(comodel_name='maneerincode.code', string="Maneerin Code")
-    name = fields.Char(string="Name")
-    age = fields.Integer(string="Age")
-    nation = fields.Char(string="Nation")
-    street = fields.Char(string="Street")
-    phone = fields.Char(string="Phone")
